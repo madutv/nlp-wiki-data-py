7efa74b7c30edfb1a0892fa7027e328112667cd5
Madhu <madhu@Madhus-MBP.fios-router.home>
2018-11-15 16:03:49 -0500

    Initial Commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+name = "nlp-data-py"
--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='nlp-data-py',
-
+    version="0.0.1",
+    author="Madhu TV",
+    author_email="madhavi.tv@gmail.com",
+    description="Create Test, Train and Validation datasets for NLP. "
+                "Currently, creating these datasets from wikipedia is supported",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
     entry_points={
         'console_scripts': [
             'peace=dataset.command_line:peace',
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
